438515c2d6aacfdd2081a1646e3d32c42a147057
marcus <marcusplay@mail.ru>
2023-12-04 17:12:40 +0300

    added example, task_1 and task_2

---
+++ task_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def garmonic_mean(*args):
+    if len(args) == 0:
+        return None
+    
+    ans = 0
+    for i in args:
+        ans += 1/i
+    
+    return (len(args)/ans)
+
+
+if __name__=="__main__":
+    a = list(map(int, input("Введите значения через пробел:\n").split()))
+    print(garmonic_mean(*a))
